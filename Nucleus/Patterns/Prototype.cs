d3809af2c75431d3f2b04ee50845d8d68aaf3baa
James Mills <jmills@tunacornstudios.com>
2024-10-18 08:05:00 +1100

    added prototypes and singletons

---
+++ Nucleus/Patterns/Prototype.cs
@@ -0,0 +1,20 @@
+﻿namespace Nucleus.Patterns
+{
+    /// <summary>
+    /// A base class that can be used for implementing the prototype pattern easily.
+    /// Contains default functionality for shallow cloning using <see cref="Object.MemberwiseClone"/>
+    /// </summary>
+    /// <typeparam name="T">The class that is implementing the prototype pattern.</typeparam>
+    public abstract class Prototype<T> where T : Prototype<T>
+    {
+        /// <summary>
+        /// Creates a shallow copy of the object using the <see cref="Object.MemberwiseClone"/> functionality
+        /// of the .NET system namespace.
+        /// </summary>
+        /// <returns>A shallow clone of the object.</returns>
+        public T ShallowClone() => (T)MemberwiseClone();
+
+        /// <returns>A deep clone of the object.</returns>
+        public abstract T DeepClone();
+    }
+}
---
+++ Nucleus/Patterns/Singleton.cs
@@ -0,0 +1,39 @@
+﻿namespace Nucleus.Patterns
+{
+    /// <summary>
+    /// A thread safe singleton type. It contains the functionality to
+    /// create singletons using generic methodology. 
+    /// </summary>
+    /// <typeparam name="T">The type being converted into a singleton.</typeparam>
+    public class Singleton<T> where T : Singleton<T>, new()
+    {
+        /// <summary>The thread safe accessor for the singleton instance.</summary>
+        public static T Instance
+        {
+            get
+            {
+                // if the instance has already been created, we can just return it.
+                if (instance != null)
+                {
+                    return instance;
+                }
+                
+                // We need to create the instance, so we'll lock the singleton on threads
+                // and create the instance.
+                lock (@lock)
+                {
+                    instance ??= new T();
+                }
+                
+                // Now that the singleton has been created, return it.
+                return instance;
+            }
+        }
+        
+        // ReSharper disable once StaticMemberInGenericType
+        /// <summary>A simple object that is used to lock threads.</summary>
+        private static readonly object @lock = new();
+        /// <summary>The internal managed singleton reference.</summary>
+        private static T? instance;
+    }
+}
